d4cea556209f4f36552fce133aa9c774ba4464db
Kaho <167737189+ninjin-cake@users.noreply.github.com>
2024-08-30 11:48:36 +0900

    Update frequency_plot.ver.5.R

--- Script/frequency_plot.ver.5.R
+++ Script/frequency_plot.ver.5.R
@@ -8,33 +8,31 @@ library(RColorBrewer)
 
 args = commandArgs(trailingOnly=T)
 
-
 ##########args##########
 #Change when using new input
-download_date <- "2024-02-19"
+download_date <- "2024-07-01"
 download.date <- as.Date(download_date)
-out_prefix <- "2024_02_19" 
-#input
-metadata.name <- "metadata.tsv"
-mut.info.name <- "metadata.mut_long.tsv"
-nextclade.name <- "nextclade.tsv"
+out_prefix <- "2024_07_01"
 
+metadata.name <- paste("/Volumes/annin-tofu/variant_monitoring/output/",out_prefix,"/metadata_tsv_",out_prefix,"/metadata.tsv",sep = "")
+mut.info.name <- paste("/Volumes/annin-tofu/variant_monitoring/output/",out_prefix,"/metadata_tsv_",out_prefix,"/metadata.mut_long.tsv",sep = "")
+nextclade.name <- paste("/Volumes/annin-tofu/variant_monitoring/output/",out_prefix,"/full.nextclade.tsv",sep = "")
+stan_f.name <- '/Users/petadimensionlab/Dropbox/github_ijampei/next_variant_detection_220413_share/method1/multinomial_independent.ver2.stan'
 #output
-pdf.observed.name <- paste(out_prefix,".method1.observed.pdf",sep="")
+out.prefix <- "output/frequency_plot"
+pdf.observed.name <- paste(out.prefix,".pdf",sep="")
 tsv.ID.name <- paste(out_prefix,".freq.id_list.csv",sep="")
-
-dir <- ""
+dir <- paste("/Volumes/annin-tofu/2405_Conference/slide_figures")
 setwd(dir)
 
 ## -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ##########parameters##########
 #general
 core.num <- 4
-#variant.ref <- "BQ.1.1" #"BA.5" #"BA.1.1" # This is now set below after the metadata has been filtered.
 
 #period to be analyzed
 date.end <- download.date
-date.start <- "2022-12-01"
+date.start <- "2021-11-01"
 
 #min numbers
 limit.count.analyzed <- 50 #was 50 then 20, now 50 again
@@ -61,11 +59,12 @@ date.end
 ##########data preprocessing & QC##########
 ###fasta file should be filtered using python script in advance
 nextclade.table <- fread(nextclade.name,header=T,sep="\t",quote="",check.names=T)
+nextclade.table <- nextclade.table %>% mutate(Virus.name = sub("\\|.*", "", seqName))
 
 metadata <- fread(metadata.name,header=T,sep="\t",quote="",check.names=T)
-mut.info <- fread(mut.info.name,header=T,sep="\t",quote="",check.names=T)
+#mut.info <- fread(mut.info.name,header=T,sep="\t",quote="",check.names=T)
 
-temp <- nextclade.table %>% left_join(metadata,by=c('seqName'='Virus.name'), relationship = "many-to-many") # Merge the nextclade and Gisaid data
+temp <- nextclade.table %>% left_join(metadata,by=c('seqName'='Virus.name')) # Merge the nextclade and Gisaid data
 temp <- temp %>% rename("Virus.name" = "seqName") %>% select(-Pango.lineage) %>% rename("Pango.lineage"="Nextclade_pango")
 metadata <- temp
 
@@ -89,23 +88,31 @@ metadata.filtered.analyzed <- metadata.filtered %>% filter(Collection.date >= da
 
 ###### using the nextclade "clade" column, we categorise all variants into 5 groups
 #plot observed
-clade.name <- read_csv("")
+metadata.filtered.analyzed.2 <- metadata.filtered.analyzed %>% select(clade_display, Collection.date)
+metadata.filtered.analyzed.2 <- metadata.filtered.analyzed.2 %>% filter(!clade_display=="") %>% mutate(clade = gsub(".*\\(([^()]*)\\).*", "\\1", clade_display))
 
-variant.top10.v <- c("XBB.1.5", "XBB.1.16", "EG.5.1", "BA.2.86", "JN.1", "other")
-clade.name.v <- c("19A","19B","20A","20B","20C","20D","20D","20E","20F","20G","20H","20I","20J","21A","21B","21C","21D","21E","21F","21G","21H",
-                  "21I","21J","21K","21L","21M","22A","22B","22C","22D","22E","22F","23A","23B","23C","23D","23E","23F","23G","23H","23I")
+##kokokara Yurinchi
+clade.interest <- c("Delta","BA.1","BA.2","BA.4","BA.5","BA.2.75","BQ.1","XBB","XBB.1.5","EG.5.1","BA.2.86","HK.3","JN.1","KP.2","KP.3","LB.1","KP.2.3","KP.3.1.1")
+colorpallet <- fread("G2P-Japan_colorpallet.txt" ,header=T,sep="\t",quote="",check.names=T)
+colorpallet <- as.data.frame(clade.interest) %>% left_join(colorpallet, by = c('clade.interest'='Pango.lineage'))
 
-metadata.filtered.analyzed.2 <- metadata.filtered.analyzed %>% filter(clade %in% clade.name.v)
-metadata.plot <- left_join(metadata.filtered.analyzed.2, clade.name, by = "clade")
+#metadata.filtered.analyzed.2 <- fread("output/metadata.filtered.analyzed.txt" ,header=T,sep="\t",quote="",check.names=T)
+metadata.filtered.analyzed.2 <- metadata.filtered.analyzed.2 %>% 
+                                mutate(col= ifelse(metadata.filtered.analyzed.2$clade %in% colorpallet$clade.interest, metadata.filtered.analyzed.2$clade, "other"))
 
-metadata.plot <- metadata.plot %>% mutate(cladename.jn1 = ifelse(cladename == "BA.2.86" & str_detect(AA.Substitutions, "Spike_L455S"), "JN.1", cladename))
+metadata.filtered.analyzed.2$col <- factor(metadata.filtered.analyzed.2$col, levels = c("other", colorpallet$clade.interest))
 
-col.v <- c(brewer.pal(length(variant.top10.v), "Paired"),"gray70")
-metadata.plot$cladename.jn1 <- factor(metadata.plot$cladename.jn1,  levels=c("other","XBB.1.5","XBB.1.16","EG.5.1","BA.2.86","JN.1"))
+colorpallet <- data.frame(clade.interest="other", col_code="#D2D1D3") %>% rbind(colorpallet)
+colorpallet <- colorpallet %>%
+               mutate(col_code = if_else(row_number() == 9, "#736357", col_code))
 
-g <- ggplot(metadata.plot,aes(x=Collection.date,fill=cladename.jn1))
-g <- g + geom_bar(stat = 'count', position = "fill")
-g <- g + scale_x_date(date_labels = "%y-%m", date_breaks = "1 months", date_minor_breaks = "1 month")
+barplot(rep(1,13), col = colorpallet$col_code) #check color
+
+col.v <- colorpallet$col_code
+
+g <- ggplot(metadata.filtered.analyzed.2,aes(x=Collection.date,fill=col))
+g <- g + geom_bar(stat = 'count', position = "fill", aes(fill=col))
+g <- g + scale_x_date(date_labels = "%y-%m", date_breaks = "3 months", date_minor_breaks = "2 month")
 g <- g + theme_set(theme_classic(base_size = 12, base_family = "Helvetica"))
 g <- g + theme(panel.grid.major = element_blank(),
                panel.grid.minor = element_blank(),
@@ -122,12 +129,4 @@ dev.off()
 
 ## -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-##### extract ID
-Id_list <- c(nextclade.table$seqName)
-Id_list <- metadata.filtered.analyzed.2 %>% filter(Virus.name %in% Id_list)
-Id_list <- c(Id_list$Accession.ID)
-
-write.csv(Id_list, file=tsv.ID.name, row.names=FALSE)
-dev.off()
-
-
+write.table(metadata.filtered.analyzed.2,"output/metadata.filtered.analyzed.txt",row.names=F,sep="\t",quote=F)
